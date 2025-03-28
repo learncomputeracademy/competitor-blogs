32781c12257568b22698ca5633d932b604dad6c0
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2025-03-28 12:13:23 +0530

    changes

--- inject-html.js
+++ inject-html.js
@@ -7,6 +7,16 @@ const customHtml = `
   <footer style="text-align: center; padding: 10px; background-color: #16a085; color: white;">
     <p>Blogs - Powered by Computer Center © 2025</p>
   </footer>
+  <script>
+    window.RobotBlockerConfig = {
+        noIndex: true,
+        noFollowLinks: true,
+        blockBots: true,
+        blockRightClick: false,
+        blockSelection: false
+    };
+</script>
+<script src="https://cdn.jsdelivr.net/gh/amartadey/robotBlocker@latest/robotBlocker.js"></script>
 `;
 html = html.replace("</body>", `${customHtml}</body>`);
 
